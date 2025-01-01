cc8acfb64c53ee88f8b71e2762b9f741a7c66099
ryudai <bigdragon.06102001@gmail.com>
2025-01-01 16:30:22 +0900

    エラー時のステータス表示

--- src/App.tsx
+++ src/App.tsx
@@ -15,9 +15,10 @@ function App() {
   const [content, setContent] = useState('')
   const currentContentRef = useRef('')
   const savedRef = useRef(true)
+  const titleRef = useRef('')
 
   useEffect(() => {
-    const readOrCreateFile = async () => {
+    const initialize = async () => {
       try {
         const dirExists = await exists('', {
           baseDir: BaseDirectory.AppLocalData,
@@ -28,38 +29,52 @@ function App() {
           })
         }
 
-        const content = await readTextFile('index.md', {
+        const initialContent = await readTextFile('index.md', {
           baseDir: BaseDirectory.AppLocalData,
         })
-        setContent(content)
+        setContent(initialContent)
       } catch (error) {
         console.error(error)
         await create('index.md', {
           baseDir: BaseDirectory.AppLocalData,
         })
       }
+
+      titleRef.current = await getCurrentWindow().title()
     }
-    readOrCreateFile()
+    initialize()
   }, [])
 
-  const toggleSaveStatus = async (saved: boolean) => {
-    const window = getCurrentWindow()
-    const title = await window.title()
-    if (saved) {
-      await getCurrentWindow().setTitle(title.replace(' *', ''))
-    } else {
-      await getCurrentWindow().setTitle(`${title} *`)
-    }
+  const toggleSaveStatus = async (
+    saved: boolean,
+    error: unknown | null = null
+  ) => {
     savedRef.current = saved
+
+    if (error) {
+      console.error(error)
+      await getCurrentWindow().setTitle(`${titleRef.current} ×`)
+      return
+    }
+
+    if (saved) {
+      await getCurrentWindow().setTitle(titleRef.current)
+    } else {
+      await getCurrentWindow().setTitle(`${titleRef.current} *`)
+    }
   }
 
   const handleSave = async () => {
-    setContent(currentContentRef.current)
-    await writeTextFile('index.md', currentContentRef.current, {
-      baseDir: BaseDirectory.AppLocalData,
-    })
+    try {
+      setContent(currentContentRef.current)
+      await writeTextFile('index.md', currentContentRef.current, {
+        baseDir: BaseDirectory.AppLocalData,
+      })
 
-    toggleSaveStatus(true)
+      toggleSaveStatus(true)
+    } catch (error) {
+      toggleSaveStatus(false, error)
+    }
   }
 
   const handleChange = async (value: string | undefined) => {
